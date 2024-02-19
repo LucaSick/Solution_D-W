909d21c7c40c364ea69767c737416b9ba47ab4da
LucaSick <lucasgvasalia@gmail.com>
2024-02-19 17:17:04 +0100

    Python solution for the task

---
+++ python_solution.py
@@ -0,0 +1,30 @@
+def getRanges(nums):
+    n = len(nums)
+    i, j = 0, 1
+    ans = ""
+
+    while i < n and j < n:
+        if nums[j] == nums[j - 1] + 1:
+            j += 1
+        else:
+            if j - i == 1:
+                ans += f"{nums[i]}, "
+            elif j - i > 1:
+                ans += f"{nums[i]}->{nums[j - 1]}, "
+            i = j
+            j += 1
+
+    if j - i == 1:
+        ans += f"{nums[i]}, "
+    elif j - i > 1:
+        ans += f"{nums[i]}->{nums[j - 1]}, "
+
+    return ans[:-2]
+
+
+input_list = input()
+if input_list == "":
+    print("No numbers provided")
+else:
+    lst = list(map(int, input_list.split()))
+    print(getRanges(lst))
